4379f229b76c02f987e45c700b62d7ef214c5228
Sumana <sumana.cr15@gmail.com>
2026-03-23 22:06:44 +0530

    Create Remainder.java

---
+++ .dist/day1/Remainder.java
@@ -0,0 +1,15 @@
+public class Remainder {
+    static int solve(int a, int b) {
+        return a % b;
+    }
+
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 6;
+
+        int result = solve(a, b);
+
+        System.out.println(result);
+    }
+    
+}
