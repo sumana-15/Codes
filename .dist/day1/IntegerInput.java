3482db176317954dec1ce984564add5116180bbc
Sumana <sumana.cr15@gmail.com>
2026-03-23 21:51:31 +0530

    Create IntegerInput.java

---
+++ .dist/day1/IntegerInput.java
@@ -0,0 +1,14 @@
+public class IntegerInput {
+    static int solve(int n) {
+        return n;
+    }
+
+    public static void main(String[] args) {
+        int n = 9;
+
+        int result = solve(n);
+
+        System.out.println(result);
+    }
+    
+}
