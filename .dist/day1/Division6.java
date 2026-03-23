7c1ff38eb54b101232a6e814eff09bb314e0d42d
Sumana <sumana.cr15@gmail.com>
2026-03-23 22:05:09 +0530

    Create Division6.java

---
+++ .dist/day1/Division6.java
@@ -0,0 +1,15 @@
+public class Division6 {
+    static int solve(int a, int b) {
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        int result = solve(a, b);
+
+        System.out.println(result);
+    }
+    
+}
