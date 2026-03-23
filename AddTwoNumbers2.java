b6f4cbff7f3e15d1e1daf54253873d06f1856c1a
Sumana <sumana.cr15@gmail.com>
2026-03-23 21:55:11 +0530

    Create AddTwoNumbers2.java

---
+++ AddTwoNumbers2.java
@@ -0,0 +1,15 @@
+public class AddTwoNumbers2 {
+
+    static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        int result = sum(a, b);
+
+        System.out.println(result);
+    }
+}
