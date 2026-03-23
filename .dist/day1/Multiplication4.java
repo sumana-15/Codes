c6a0128f47e8257d8ae65a60de579f99ecd076d7
Sumana <sumana.cr15@gmail.com>
2026-03-23 22:02:06 +0530

    Create Multiplication4.java

---
+++ .dist/day1/Multiplication4.java
@@ -0,0 +1,15 @@
+public class Multiplication4 {
+
+    static int solve(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        int result = solve(a, b);
+
+        System.out.println(result);
+    }
+}
