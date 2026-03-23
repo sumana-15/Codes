995e5dc3b79183bd1780862e3b5ee78bcb7cc43a
Sumana <sumana.cr15@gmail.com>
2026-03-23 22:03:41 +0530

    Create Subtraction5.java

---
+++ .dist/day1/Subtraction5.java
@@ -0,0 +1,15 @@
+public class Subtraction5 {
+    static int solve(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 75;
+
+        int result = solve(a, b);
+
+        System.out.println(result);
+    }
+    
+}
