e4c1df2a9a8bf5999fbf84c53748b5df9c9ee58d
Sumana <sumana.cr15@gmail.com>
2026-03-23 22:17:04 +0530

    updated

---
+++ .dist/day1/AverageOfTwoNumbers.java
@@ -0,0 +1,16 @@
+public class AverageOfTwoNumbers {
+    static int solve(int a, int b) {
+        return (a + b) / 2;
+    }
+
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 20;
+
+        int result = solve(a, b);
+
+        System.out.println(result);
+    }
+    
+}
