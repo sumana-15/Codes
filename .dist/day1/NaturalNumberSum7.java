15e34aaf6cbbf2aeec479d1081acd00e72435c2d
Sumana <sumana.cr15@gmail.com>
2026-03-23 22:09:20 +0530

    Create NaturalNumberSum7.java

---
+++ .dist/day1/NaturalNumberSum7.java
@@ -0,0 +1,15 @@
+public class NaturalNumberSum7 {
+    static int solve(int n) {
+        return n * (n + 1) / 2;
+    }
+
+    public static void main(String[] args) {
+        
+        int n = 6;
+
+        int result = solve(n);
+
+        System.out.println(result);
+    }
+    
+}
