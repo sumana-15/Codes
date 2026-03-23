7de1c692917a44ae5084b9530b6c2a14d54bd729
Sumana <sumana.cr15@gmail.com>
2026-03-23 21:58:04 +0530

    Create SumOfThreeNumbers3.java

---
+++ .dist/day1/SumOfThreeNumbers3.java
@@ -0,0 +1,17 @@
+public class SumOfThreeNumbers3 {
+    static int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = 15;
+
+
+        int result = sum(a, b, c);
+
+        System.out.println(result);
+    }
+    
+}
