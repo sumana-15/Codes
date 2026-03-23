c9e8f793d075122c04995217e630d797e6cb0d9e
Sumana <sumana.cr15@gmail.com>
2026-03-23 22:30:41 +0530

    Create LeapYear.java

---
+++ .dist/day2/LeapYear.java
@@ -0,0 +1,17 @@
+public class Leapyear {
+    static boolean check(int year){
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
+
+    public static void main(String[] args) {
+        int year = 2024;
+
+        if(check(year)){
+            System.out.println("Leap Year");
+        } else {
+            System.out.println("Not a Leap Year");
+        }
+    }
+
+    
+}
