e8c5e403e922206e099cdd84ae79a4ab51a5f4bb
Sumana <sumana.cr15@gmail.com>
2026-03-24 22:43:47 +0530

    Update MultipleOfThree.java

--- .dist/day2/MultipleOfThree.java
+++ .dist/day2/MultipleOfThree.java
@@ -8,7 +8,7 @@ public class MultipleOfThree {
         int n = 9;
 
         if(check(n)){
-            System.out.println("Multiple  3");
+            System.out.println("Multiple of  3");
         } else {
             System.out.println("Not a Multiple of 3");
         }
