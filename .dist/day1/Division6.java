3822c8e9510b35560b7c0bfb0d369d719e6ee48e
Sumana <sumana.cr15@gmail.com>
2026-03-31 21:39:52 +0530

    Update Division6.java

--- .dist/day1/Division6.java
+++ .dist/day1/Division6.java
@@ -15,6 +15,7 @@ public class Division6 {
         System.out.println(result);
         System.out.println(result);
         System.out.println(result);
+        System.out.println(result);
         
         
         
