8508ac18927f4c14666d8cc4a2405046fa8677fc
Sumana <sumana.cr15@gmail.com>
2026-04-07 23:08:44 +0530

    Update Remainder.java

--- .dist/day1/Remainder.java
+++ .dist/day1/Remainder.java
@@ -31,6 +31,7 @@ public class Remainder {
         System.out.println(result);
         System.out.println(result);
         System.out.println(result);
+        System.out.println(result);
         
        
 
