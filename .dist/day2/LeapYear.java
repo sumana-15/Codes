71427a1f7ead180999d60214c8c277c9c811d729
Sumana <sumana.cr15@gmail.com>
2026-04-09 20:14:46 +0530

    Update LeapYear.java

--- .dist/day2/LeapYear.java
+++ .dist/day2/LeapYear.java
@@ -19,6 +19,7 @@ public class LeapYear {
             System.out.println("Not a Leap Year");
             System.out.println("Not a Leap Year");
             System.out.println("Not a Leap Year");
+            System.out.println("Not a Leap Year");
         }
     }
 }
