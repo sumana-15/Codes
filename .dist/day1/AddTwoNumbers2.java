da00a83105e4eb18b0ffa7a5d2f92f0197cf1e26
Sumana <sumana.cr15@gmail.com>
2026-04-09 20:06:18 +0530

    Update AddTwoNumbers2.java

--- .dist/day1/AddTwoNumbers2.java
+++ .dist/day1/AddTwoNumbers2.java
@@ -34,8 +34,7 @@ public class AddTwoNumbers2 {
         System.out.println(result);
         System.out.println(result);
         System.out.println(result);
-        System.out.println(result);
-        
+        System.out
 
 
 
