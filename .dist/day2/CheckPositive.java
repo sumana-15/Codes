4dfa33dd7769a21fc745aa74a2f60ab6ed12b52a
Sumana <sumana.cr15@gmail.com>
2026-04-20 20:07:05 +0530

    Update CheckPositive.java

--- .dist/day2/CheckPositive.java
+++ .dist/day2/CheckPositive.java
@@ -21,7 +21,6 @@ public class CheckPositive {
             System.out.println("Not Positive");
             System.out.println("Not Positive");
             System.out.println("Not Positive");
-            System.out.println("Not Positive");
             
         }
     }
