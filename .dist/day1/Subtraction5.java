5547b399a2cc19532c1021000f2ed8d7b77855e1
Sumana <sumana.cr15@gmail.com>
2026-04-21 22:22:15 +0530

    Update Subtraction5.java

--- .dist/day1/Subtraction5.java
+++ .dist/day1/Subtraction5.java
@@ -29,6 +29,7 @@ public class Subtraction5 {
         System.out.println(result);
         System.out.println(result);
         System.out.println(result);
+        System.out.println(result);
         
     
         
