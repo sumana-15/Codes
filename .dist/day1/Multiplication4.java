5d517b2582a91eb322901ca2997e2772a1ad9d25
Sumana <sumana.cr15@gmail.com>
2026-05-25 23:30:58 +0530

    Update Multiplication4.java

--- .dist/day1/Multiplication4.java
+++ .dist/day1/Multiplication4.java
@@ -33,7 +33,6 @@ public class Multiplication4 {
         System.out.println(result);
         System.out.println(result);
         System.out.println(result);
-        System.out.println(result);
         
         
 
