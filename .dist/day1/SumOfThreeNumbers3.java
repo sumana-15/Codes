75dd71ec1de5fff455dbb4592d942b9dbda1d900
Sumana <sumana.cr15@gmail.com>
2026-05-30 23:19:57 +0530

    Update SumOfThreeNumbers3.java

--- .dist/day1/SumOfThreeNumbers3.java
+++ .dist/day1/SumOfThreeNumbers3.java
@@ -39,7 +39,6 @@ public class SumOfThreeNumbers3 {
         System.out.println(result);
         System.out.println(result);
         System.out.println(result);
-        System.out.println(result);
         
 
         
