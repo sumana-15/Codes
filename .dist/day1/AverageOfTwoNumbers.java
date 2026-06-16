a942419ce23b9be9f4148bf355ddcfb0a1559802
Sumana <sumana.cr15@gmail.com>
2026-06-16 22:23:57 +0530

    Update AverageOfTwoNumbers.java

--- .dist/day1/AverageOfTwoNumbers.java
+++ .dist/day1/AverageOfTwoNumbers.java
@@ -18,7 +18,6 @@ public class AverageOfTwoNumbers {
         System.out.println(result);
         System.out.println(result);
         System.out.println(result);
-        System.out.println(result);
     }
     
 }
