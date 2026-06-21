e922a5a7a6e4820878bdbdf59a9c2361c5a0976c
Sumana <sumana.cr15@gmail.com>
2026-06-21 23:35:08 +0530

    Update IntegerInput.java

--- .dist/day1/IntegerInput.java
+++ .dist/day1/IntegerInput.java
@@ -29,6 +29,7 @@ public class IntegerInput {
        System.out.println(result);
        System.out.println(result);
        System.out.println(result);
+       System.out.println(result);
        System.out.println(result); 
        
        
