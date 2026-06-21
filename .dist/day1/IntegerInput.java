f463ecfad683c8d5240981ce370856131763a012
Sumana <sumana.cr15@gmail.com>
2026-06-21 23:31:05 +0530

    Update IntegerInput.java

--- .dist/day1/IntegerInput.java
+++ .dist/day1/IntegerInput.java
@@ -8,7 +8,8 @@ public class IntegerInput {
 
         int result = solve(n);
 
-       System.out.println(result); 
+       System.out.println(result);
+       System.out.println(result);  
        
        
        
