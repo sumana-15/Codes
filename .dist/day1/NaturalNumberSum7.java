439dffa8f2ef907b45f6910e82919687f60e4abd
Sumana <sumana.cr15@gmail.com>
2026-07-25 20:13:32 +0530

    Update NaturalNumberSum7.java

--- .dist/day1/NaturalNumberSum7.java
+++ .dist/day1/NaturalNumberSum7.java
@@ -16,6 +16,8 @@ public class NaturalNumberSum7 {
         System.out.println(result);
         System.out.println(result);
         System.out.println(result);
+        System.out.println(result);
+
 
         
     }
